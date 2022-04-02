d9fdef78ba6c8c0cf08507aae9cccb2f12d0071b
Scott Lambert <sdlambert@users.noreply.github.com>
2022-04-01 20:16:38 -0400

    Rename store for new OpenLibrary books

--- resources/js/store/index.js
+++ resources/js/store/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 import { createLogger } from 'vuex'
-import newBook from './modules/newOpenLibraryBook';
+import newOpenLibraryBook from './modules/newOpenLibraryBook';
 
 Vue.use(Vuex);
 
@@ -9,7 +9,7 @@ const debug = process.env.NODE_ENV !== 'production'
 
 export default new Vuex.Store({
   modules: {
-    newBook
+    newOpenLibraryBook
   },
   strict: debug,
   plugins: debug ? [createLogger()] : []
