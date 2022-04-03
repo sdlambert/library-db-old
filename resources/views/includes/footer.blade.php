43b63997e7e84d7c2cf37f49ed8de304b6f3ea90
Scott Lambert <sdlambert@users.noreply.github.com>
2022-04-03 13:55:29 -0400

    Add a footer
    * Fix an overflow issue with the scroll-to-top button

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,4 +1,13 @@
 <footer>
+    <div class="row">
+        <div class="col-12">
+            <ul class="unstyled">
+                <li><a href="/books">Books</a></li>
+                <li><a href="/authors">Authors</a></li>
+            </ul>
+            <p></p>
+        </div>
+    </div>
     <button id="scroll-to-top" class="is-fixed is-hidden button primary">
         <svg height="48" width="48" xmlns="http://www.w3.org/2000/svg">
             <path d="M14.83 30.83 24 21.66l9.17 9.17L36 28 24 16 12 28z"/>
